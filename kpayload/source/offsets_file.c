a85bd5489ac071c412b46eac013bce4a86c30551
Al-Azif <alazif@miskatonic.io>
2025-06-24 15:03:24 -0700

    Finally fix #1
    I feel stupid for making this so complicated

--- kpayload/source/offsets_file.c
+++ kpayload/source/offsets_file.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 
+#include "offsets.h"
 #include "sections.h"
 
 // #include "offsets/474.h"
@@ -48,77 +49,33 @@
 #include "offsets/1152.h"
 #include "offsets/1200.h"
 #include "offsets/1202.h"
-
-struct fw_offsets_entry {
-  uint16_t fw_version;
-  const struct kpayload_offsets *offsets;
-};
-
-static const struct fw_offsets_entry offsets_table[] PAYLOAD_RDATA = {
-  // { 474, &offsets_474 }
-  // { 500, &offsets_500 },
-  // { 501, &offsets_501 },
-  // { 503, &offsets_503 },
-  // { 505, &offsets_505 },
-  // { 507, &offsets_507 },
-  // { 550, &offsets_550 },
-  // { 553, &offsets_553 },
-  // { 555, &offsets_555 },
-  // { 556, &offsets_556 },
-  // { 600, &offsets_600 },
-  // { 602, &offsets_602 },
-  // { 620, &offsets_620 },
-  // { 650, &offsets_650 },
-  // { 651, &offsets_651 },
-  // { 670, &offsets_670 },
-  // { 671, &offsets_671 },
-  // { 672, &offsets_672 },
-  // { 700, &offsets_700 },
-  // { 701, &offsets_701 },
-  // { 702, &offsets_702 },
-  // { 750, &offsets_750 },
-  // { 751, &offsets_751 },
-  // { 755, &offsets_755 },
-  // { 800, &offsets_800 },
-  // { 801, &offsets_801 },
-  // { 803, &offsets_803 },
-  // { 850, &offsets_850 },
-  // { 852, &offsets_852 },
-  // { 900, &offsets_900 },
-  // { 903, &offsets_903 },
-  // { 904, &offsets_904 },
-  // { 950, &offsets_950 },
-  // { 951, &offsets_951 },
-  // { 960, &offsets_960 },
-  // { 1000, &offsets_1000 },
-  // { 1001, &offsets_1001 },
-  // { 1050, &offsets_1050 },
-  // { 1070, &offsets_1070 },
-  // { 1071, &offsets_1071 },
-  { 1100, &offsets_1100 },
-  { 1102, &offsets_1102 },
-  { 1150, &offsets_1150 },
-  { 1152, &offsets_1152 },
-  // { 1200, &offsets_1200 },
-  // { 1202, &offsets_1202 },
-};
+// #include "offsets/1250.h"
 
 PAYLOAD_CODE const struct kpayload_offsets *get_offsets_for_fw(uint16_t fw_version) {
-  const size_t table_size = sizeof(offsets_table) / sizeof(offsets_table[0]);
-
-  if (table_size == 0) {
-    return NULL;
+  switch (fw_version) {
+    case 1000:
+      return &offsets_1000;
+    case 1001:
+      return &offsets_1001;
+    case 1050:
+      return &offsets_1050;
+    case 1070:
+      return &offsets_1070;
+    case 1071:
+      return &offsets_1071;
+    case 1100:
+      return &offsets_1100;
+    case 1102:
+      return &offsets_1102;
+    case 1150:
+      return &offsets_1150;
+    case 1152:
+      return &offsets_1152;
+    case 1200:
+      return &offsets_1200;
+    case 1202:
+      return &offsets_1202;
+    default:
+      return NULL;
   }
-
-  for (size_t i = 0; i < table_size; i++) {
-    if (offsets_table[i].offsets == NULL) {
-      continue;
-    }
-
-    if (offsets_table[i].fw_version == fw_version) {
-      return offsets_table[i].offsets;
-    }
-  }
-
-  return NULL;
 }
