69eee80632dfe85aa6500160450a57959216ba39
illusiony <37698908+illusion0001@users.noreply.github.com>
2025-07-02 22:00:27 +1200

    Upload config to disk

--- installer/source/config.c
+++ installer/source/config.c
@@ -15,10 +15,13 @@
 #define DEFAULT_UPLOAD_PRX 1
 #define DEFAULT_ENABLE_PLUGINS 1
 
+#include "hen.ini.inc.c"
+
 #define MATCH(n) strcmp(name, n) == 0
 
 static void upload_ver(void) {
   write_blob(BASE_PATH "/" VERSION_TXT, VERSION, sizeof(VERSION) - 1);
+  write_blob(HDD_INI_PATH, hen_ini, hen_ini_len);
 }
 
 // Helper function to set all configuration values to their defaults
