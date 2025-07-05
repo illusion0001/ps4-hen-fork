5c902b09075217c285d73654d4507750dbdb0f44
illusiony <37698908+illusion0001@users.noreply.github.com>
2025-07-05 16:08:31 +1200

    Check version field

--- installer/include/config.h
+++ installer/include/config.h
@@ -3,6 +3,11 @@
 
 #include "config_struct.h"
 
+#define DEFAULT_CONFIG_VERSION 1
+
+void upload_ini(const char *path);
 int init_config(struct configuration *config);
+
+extern int found_version;
 
 #endif
