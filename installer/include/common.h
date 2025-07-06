7cc337efc1027447d9be90f17a3bec3b3114ce0c
Al Azif <33132478+Al-Azif@users.noreply.github.com>
2025-07-05 19:17:19 -0700

    Chunk and zlib PRX files

--- installer/include/common.h
+++ installer/include/common.h
@@ -4,6 +4,9 @@
 #include <types.h>
 
 void write_blob(const char *path, const void *blob, const size_t blobsz);
+const unsigned char *decompress_zlib(const unsigned char *zlib_data, size_t zlib_len, size_t *out_size);
+const unsigned char *decompress_chunked_zlib(const unsigned char **chunk_ptrs, const size_t *chunk_lens, const size_t num_chunks, const size_t expected_decompressed_len);
+void unchunk_decompress_and_write(const unsigned char **chunk_ptrs, const size_t *chunk_lens, const size_t num_chunks, const size_t expected_decompressed_len, const char *out_path);
 void kill_proc(const char *proc);
 void block_updates(void);
 
