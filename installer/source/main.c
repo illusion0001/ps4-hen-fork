ec56cda7c66b1192045545e338b4037e9e4c28c2
Al-Azif <alazif@miskatonic.io>
2025-07-06 00:01:42 -0700

    init lib

--- installer/source/main.c
+++ installer/source/main.c
@@ -97,6 +97,11 @@ int _main(struct thread *td) {
   const int u_to_sec = 1000 * 1000;
   initKernel();
   initLibc();
+  initModule();
+  const int ORBIS_SYSMODULE_ZLIB = 0x00C5;
+  if (sceSysmoduleLoadModule(ORBIS_SYSMODULE_ZLIB) == 0) {
+    initZlib();
+  }
 
 #ifdef DEBUG_SOCKET
   initNetwork();
